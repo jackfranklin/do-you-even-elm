2605d29bc66911234790cbf566f628e83dab0b02
Jack Franklin <jack@jackfranklin.net>
2016-09-09 10:02:40 +0200

    Initial commit

--- TestBrowserRunner.elm
+++
@@ -1,9 +0,0 @@
-module BrowserTestRunner exposing (..)
-
-import ElmTest exposing (runSuiteHtml)
-import Tests
-
-
-main : Program Never
-main =
-    runSuiteHtml Tests.all
--- TestRunner.elm
+++
@@ -1,9 +0,0 @@
-module TestRunner exposing (..)
-
-import ElmTest exposing (runSuite)
-import Tests
-
-
-main : Program Never
-main =
-    runSuite Tests.all
