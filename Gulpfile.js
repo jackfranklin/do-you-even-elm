2d6ff4fffe89cfec5a186f4f3a6e7a9b66220bf7
Jack Franklin <jack@jackfranklin.net>
2016-09-18 19:55:26 +0100

    Deploy to surge!

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var liveServer = require('live-server');
 var $ = require('gulp-load-plugins')({});
+var del = require('del');
 
 function watchElmAndRun(...args) {
   return gulp.watch('**/*.elm', args);
@@ -13,6 +14,34 @@ gulp.task('build', function() {
     }))
     .pipe($.elm.bundle('App.js', { warn: true }))
     .pipe(gulp.dest('build/'));
+});
+
+gulp.task('prod:elm', ['prod:clean'], function() {
+  return gulp.src('App.elm')
+    .pipe($.elm.bundle('App.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('dist/build'));
+});
+
+gulp.task('prod:clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('prod:vendor', ['prod:clean'], function() {
+  return gulp.src('vendor/*').pipe(gulp.dest('dist/vendor'));
+});
+
+gulp.task('prod:html', ['prod:clean'], function() {
+  return gulp.src('index.html').pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-prod', ['prod:vendor', 'prod:html', 'prod:elm']);
+
+gulp.task('deploy', ['build-prod'], function() {
+  return $.surge({
+    project: './dist',
+    domain: 'doyouevenelm.com'
+  });
 });
 
 gulp.task('start', ['build'], function() {
