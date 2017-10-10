b3ab6f3d71acac305b472310258f33a8b7b6ebf8
Jack Franklin <jack@jackfranklin.net>
2017-10-09 21:09:27 -0700

    set up prettier for JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import { Main } from './Main.elm';
-console.log('env', process.env);
-Main.embed(document.getElementById('root'), {
+import { Main } from './Main.elm'
+const app = Main.embed(document.getElementById('root'), {
   githubToken: process.env.ELM_APP_GITHUB_TOKEN,
-});
+})
